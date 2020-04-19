eadca6b88d75b148ddb8313a5adebe8432fc6574
mcbobby <madumcbobby@yahoo.com>
2020-04-19 16:41:40 +0100

    handling booking request

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'it works'
-    });
-});
+const bookingRoute = require('./api/routes/booking')
+
+app.use('/bookings', bookingRoute);
 
 module.exports = app;
