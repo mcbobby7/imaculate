62979dfc5dc6604bad9b91051a8f3eda45a07a83
mcbobby <madumcbobby@yahoo.com>
2020-04-19 18:11:08 +0100

    handle errors

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ const bookingRoute = require('./api/routes/booking')
 
 app.use('/bookings', bookingRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
