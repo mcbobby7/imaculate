c7a3c1fd8353669a4488aec0312b54385753d079
mcbobby <madumcbobby@yahoo.com>
2020-04-19 18:42:51 +0100

    parsing body

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price,
+    };
     res.status(200).json({
-        message: 'handling post'
+        message: 'handling post',
+        createdProperty: product
     });
 });
 
--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const bookingRoute = require('./api/routes/booking')
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requestted-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', '*');
+        return res.status(200).json({})
+    }
+})
 
 app.use('/bookings', bookingRoute);
 
