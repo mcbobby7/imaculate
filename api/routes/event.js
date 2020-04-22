57f7774a167fcff142b0733adb8070ef947b95e5
mcbobby <madumcbobby@yahoo.com>
2020-04-22 20:26:19 +0100

    add get event code to fetch all event

--- api/routes/event.js
+++ api/routes/event.js
@@ -1,20 +1,51 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const Event = require('../models/event');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling Get'
-    });
+    Event.find()
+    .select("title, description price date creator _id")
+    .exec()
+    .then(docs => {
+        const response = {
+            count: docs.length,
+            events: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    title: doc.title,
+                    description: doc.description,
+                    price: doc.price,
+                    date: doc.price,
+                    creator: doc.creator,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/events/" + doc._id
+                    }
+                }
+            })
+        }
+    })
+    res.status(200).json(response);
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
+    const event = new Event({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        description: req.body.description,
         price: req.body.price,
-    };
+        date: req.body.price,
+        creator: req.body.creator,
+    });
+    event.save().then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+
     res.status(200).json({
-        message: 'handling post',
-        createdProperty: product
+        message: "event created successfully",
+        createdProperty: {event}
     });
 });
 
