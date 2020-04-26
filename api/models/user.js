14252a0e151194b8453a7a29e6e391a77c9d5877
mcbobby <madumcbobby@yahoo.com>
2020-04-26 15:39:27 +0100

    improve email validation

--- api/models/user.js
+++ api/models/user.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     firstName: {
         type: String,
