5ebb150201b124386f012055c1dcddde7d86c323
mcbobby <madumcbobby@yahoo.com>
2020-04-26 15:27:40 +0100

    add user signup route and algorithm

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const bcrypt = require("bcrypt")
 const User = require('../models/user');
+
+
+router.post('/signup', (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user => 1) {
+            return res.status(409).json({
+                message: 'user already exist'
+            });
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                    .then(result => {
+                        res.status(201).json({
+                            message: "User created successfuly"
+                        });
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+                }
+            });
+        }
+    });
+});
 
 router.get('/', (req, res, next) => {
     User.find()
