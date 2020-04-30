610765ea4777c7dc1952e281f6fc3476f4302b24
mcbobby <madumcbobby@yahoo.com>
2020-04-30 21:17:29 +0100

    clean up code

--- api/routes/booking.js
+++ api/routes/booking.js
@@ -4,12 +4,6 @@ const checkAuth = require('../middleware/check-auth');
 const Booking_controller = require('../controllers/bookings')
 
 
-// router.get('/', checkAuth, (req, res, next) => {
-//     res.status(200).json({
-//         message: 'handling Get'
-//     });
-// });
-
 router.post('/', checkAuth, Booking_controller.post_booking );
 
 router.delete('/:bookingId', checkAuth, Booking_controller.deleteBooking );
