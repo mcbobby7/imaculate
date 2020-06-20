4bfddb3f94634edc0a699bdcd5a9ffb1456d6353
mcbobby <madumcbobby@yahoo.com>
2020-06-20 21:23:01 +0100

    Update blog.js

--- api/routes/blog.js
+++ api/routes/blog.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const Blog_controller = require('../controllers/blog')
 
-router.get('/', checkAuth, Blog_controller.get_blogs);
+router.get('/', Blog_controller.get_blogs);
 
-router.post('/', checkAuth, Blog_controller.post_blog );
+router.post('/', Blog_controller.post_blog );
 
-router.get('/:eventId', checkAuth, Blog_controller.single_blog);
+router.get('/:blogId', Blog_controller.single_blog);
 
 router.patch('/:blogId', checkAuth, Blog_controller.patch_blog);
 
