a27ea53ef7dd498b485c891c1037168542745b51
mcbobby <madumcbobby@yahoo.com>
2020-06-27 14:06:32 +0100

    add upload image function

--- api/controllers/blog.js
+++ api/controllers/blog.js
@@ -1,5 +1,32 @@
 const mongoose = require('mongoose');
 const Blog = require('../models/blog');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+  
+  const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+  
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
 
 
 exports.post_blog = (req, res, next) => {
@@ -10,6 +37,8 @@ exports.post_blog = (req, res, next) => {
         imgUrl: req.body.imgUrl,
         date: req.body.date,
         tag: req.body.tag,
+        image: req.file.path,
+
     });
     blog.save().then(result => {
         res.status(200).json({
@@ -21,6 +50,7 @@ exports.post_blog = (req, res, next) => {
                 imgUrl: result.imgUrl,
                 date: result.data,
                 tag: result.tag,
+                image: result.image,
                 request: {
                     type: "GET, PATCH, DELETE",
                     url: "http://localhost:4000/blog/" + result._id
--- api/routes/blog.js
+++ api/routes/blog.js
@@ -2,10 +2,37 @@ const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const Blog_controller = require('../controllers/blog')
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+  
+  const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+  
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
 
 router.get('/', Blog_controller.get_blogs);
 
-router.post('/', Blog_controller.post_blog );
+router.post('/', upload.single('image'), Blog_controller.post_blog );
 
 router.get('/:blogId', Blog_controller.single_blog);
 
