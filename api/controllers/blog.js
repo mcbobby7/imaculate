372b49480e0842819079877fa92e9f1de300d87c
mcbobby <madumcbobby@yahoo.com>
2020-06-27 14:48:49 +0100

    Update blog.js

--- api/controllers/blog.js
+++ api/controllers/blog.js
@@ -2,32 +2,6 @@ const mongoose = require('mongoose');
 const Blog = require('../models/blog');
 const multer = require('multer');
 
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb) {
-//       cb(null, 'uploads/');
-//     },
-//     filename: function(req, file, cb) {
-//       cb(null, new Date().toISOString() + file.originalname);
-//     }
-//   });
-  
-//   const fileFilter = (req, file, cb) => {
-//     // reject a file
-//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//       cb(null, true);
-//     } else {
-//       cb(null, false);
-//     }
-//   };
-  
-//   const upload = multer({
-//     storage: storage,
-//     limits: {
-//       fileSize: 1024 * 1024 * 5
-//     },
-//     fileFilter: fileFilter
-//   });
-
 
 exports.post_blog = (req, res, next) => {
     const blog = new Blog({
@@ -40,7 +14,6 @@ exports.post_blog = (req, res, next) => {
 
     });
     blog.save().then(result => {
-        console.log(result);
         res.status(200).json({
             message: "blog created successfully",
             createdProperty: {
